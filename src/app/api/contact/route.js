99edc271b2ad4fbd54363be095c8f0fe33daaff8
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-28 14:34:36 +0530

    make contact us form live.

---
+++ src/app/api/contact/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { name, email, contact, message } = body;
+
+    if (!name || !email || !contact || !message) {
+      return NextResponse.json(
+        { message: "Please fill all the fields" },
+        { status: 400 }
+      );
+    }
+
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["priyeshrai.dev@gmail.com"],
+      subject: "Contact Me Form",
+      html: `<strong>Name: ${name} <br/> 
+            Email: ${email} <br/> 
+            Contact No: ${contact} <br/>
+            Message: ${message} <br/> </strong>`,
+    });
+
+    if (error) {
+      return NextResponse.json(
+        { error: error.message || "Error sending email" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Email sent successfully", data },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    return NextResponse.json(
+      { error: "An error occurred while processing the request." },
+      { status: 500 }
+    );
+  }
+}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,65 @@
+'use client'
 import { IconSend } from '@tabler/icons-react'
-import React from 'react'
+import React, { useState } from 'react'
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Contact() {
+    const [contactData, setContactData] = useState({
+        name: "",
+        email: "",
+        contact: "",
+        message: "",
+    });
+
+    async function handleChange(event) {
+        event.preventDefault();
+        const { name, value } = event.target;
+
+        setContactData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            toast.promise(
+                fetch("/api/contact", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(contactData),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log("Message sent successfully", data);
+                        if (data.status === "success") {
+                            setContactData({
+                                name: "",
+                                email: "",
+                                contact: "",
+                                message: "",
+                            });
+                        }
+                    })
+                    .catch((error) => {
+                        console.log("Error sending message", error.message);
+                        throw error;
+                    }),
+                {
+                    loading: "Sending message...",
+                    success: "Message sent successfully!",
+                    error: "Error sending message. Please try again.",
+                }
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <section className='section mt-20 mb-24' id='contact'>
             <div className="section-wraper">
@@ -21,24 +79,38 @@ export default function Contact() {
 
                     <div className='relative w-full mt-5 flex flex-col gap-4'>
                         <div className='flex-1 relative bg-[#252526] rounded-lg '>
-                            <input type="text" placeholder='Eg - Priyessh rai' spellCheck required name='name' className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
+                            <input type="text" placeholder='Eg - Priyessh rai' spellCheck required name='name'
+                                onChange={handleChange}
+                                value={contactData.name}
+                                className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
                         </div>
                         <div className='flex-1 relative bg-[#252526] rounded-lg '>
-                            <input type="email" placeholder='Eg - priyesshrai@gmail.com' spellCheck required name='email' className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
+                            <input type="email" placeholder='Eg - priyesshrai@gmail.com' spellCheck required name='email'
+                                onChange={handleChange}
+                                value={contactData.email}
+                                className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
                         </div>
                         <div className='flex-1 relative bg-[#252526] rounded-lg '>
-                            <input type="tel" placeholder='Eg - +91 9847623236' required name='contact' className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
+                            <input type="tel" placeholder='Eg - +91 9847623236' required name='contact'
+                                onChange={handleChange}
+                                value={contactData.contact}
+                                className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1' />
                         </div>
                         <div className='flex-1 relative bg-[#252526] rounded-lg '>
-                            <textarea placeholder='Messgae' required spellCheck name='message' className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1 h-40 ' />
+                            <textarea placeholder='Message' required spellCheck name='message'
+                                onChange={handleChange}
+                                value={contactData.message}
+                                className='relative w-full bg-transparent px-4 py-3 rounded-lg outline-none border border-transparent font-jura text-xl text-[#dadada] focus:border-yellow-300 focus:border-1 h-40 ' />
                         </div>
                         <div className='flex-1 relative flex justify-center align-middle '>
-                            <button className='w-full leading-none px-16 py-3 bg-white text-[20px] text-black rounded-lg flex align-middle justify-center hover:bg-yellow-300 transition-all gap-2'>Send <IconSend className='inline-block w-5' /> </button>
+                            <button className='w-full leading-none px-16 py-3 bg-white text-[20px] text-black rounded-lg flex align-middle justify-center hover:bg-yellow-300 transition-all gap-2'
+                                onClick={handleSubmit}
+                            >Send <IconSend className='inline-block w-5' /> </button>
                         </div>
-
                     </div>
                 </div>
             </div>
+            <Toaster />
         </section>
-    )
+    );
 }
